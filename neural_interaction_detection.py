106f7005a4874d188526ebad3e766ff7db3e371a
fischkas <kasperfischer1993@gmail.com>
2025-08-05 12:59:27 +0200

    Update neural_interaction_detection.py

--- neural_interaction_detection.py
+++ neural_interaction_detection.py
@@ -11,7 +11,7 @@ import pickle
 
 
 for i in range(1, 101):
-    model_path = "/home/kasper/nas/model_runs/mlp_hos6_clumped/model/model_" + str(i) + ".pth"
+    model_path = "/path/to/models/model_" + str(i) + ".pth"
     model = torch.load(model_path)
     model_weights = get_weights(model)
     
@@ -20,14 +20,14 @@ for i in range(1, 101):
     pairwise = get_nlargest(generator = interpret_pairwise_interactions(w_input, w_later), N = 100)
     pairwise_pd = pd.DataFrame(pairwise, columns=["Index", "Importance"])
     
-    pair_path = "/home/kasper/nas/model_runs/mlp_hos6_clumped/nid_ouput/pairwise/model_" + str(i) + ".csv"
+    pair_path = "/path/to/pairs/model_" + str(i) + ".csv"
     pairwise_pd.to_csv(pair_path)
     
     ### Higher order interactions
     higher = get_interactions(model_weights)
     higher_pd = pd.DataFrame(higher, columns=["Index", "Importance"])
     
-    higher_path = "/home/kasper/nas/model_runs/mlp_hos6_clumped/nid_ouput/higher/model_" + str(i) + ".csv"
+    higher_path = "/path/to/higher/model_" + str(i) + ".csv"
     higher_pd.to_csv(higher_path)
 
 
